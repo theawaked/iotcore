9a0a8910ab54fda32b813227ac3d4a3b1435ff76
gerben bunt <g.bunt@st.hanze.nl>
2019-11-05 23:28:12 +0100

    main functie

--- tempsensor.py
+++ tempsensor.py
@@ -10,14 +10,22 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus,address)
 
-while True:
-    bme280_data = bme280.sample(bus,address)
+def readandlog():
+          
+          bme280_data = bme280.sample(bus,address)
 
-    temperature = bme280_data.temperature
-    humidity  = bme280_data.humidity
-    pressure  = bme280_data.pressure
-    
-    print(temperature, humidity)
-    database.insertdata(temperature, humidity)
-    database.printdb()
-    sleep(20)
+          temperature = bme280_data.temperature
+          humidity  = bme280_data.humidity
+          pressure  = bme280_data.pressure
+          
+          print(temperature, humidity)
+          database.insertdata(temperature, humidity)
+          database.printdb()
+          
+
+def main():
+          while True:
+                    readandlog()
+                    sleep(20)
+
+main()
