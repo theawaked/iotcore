ce720fc6f1674032229fb9cd4bf6c726c1acdff4
gerben bunt <g.bunt@st.hanze.nl>
2019-11-05 22:27:45 +0100

    wer

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ con = lite.connect('sensorData.db')
 with con: 
     cur = con.cursor() 
     #cur.execute("DROP TABLE IF EXISTS DHT_data")
-    cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, temp NUMERIC, hum NUMERIC)")
+    #cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, temp NUMERIC, hum NUMERIC)")
 
     cur.execute("INSERT INTO DHT_data VALUES(datetime('now'), 20.5, 30)")
     cur.execute("INSERT INTO DHT_data VALUES(datetime('now'), 25.8, 40)")
