8f923ff5b21100f2bc52c7954b3dd87195942d5c
gerben bunt <g.bunt@st.hanze.nl>
2019-11-05 22:26:49 +0100

    table

--- database.py
+++ database.py
@@ -6,3 +6,7 @@ with con:
     cur = con.cursor() 
     #cur.execute("DROP TABLE IF EXISTS DHT_data")
     cur.execute("CREATE TABLE DHT_data(timestamp DATETIME, temp NUMERIC, hum NUMERIC)")
+
+    cur.execute("INSERT INTO DHT_data VALUES(datetime('now'), 20.5, 30)")
+    cur.execute("INSERT INTO DHT_data VALUES(datetime('now'), 25.8, 40)")
+    cur.execute("INSERT INTO DHT_data VALUES(datetime('now'), 30.3, 50)")
