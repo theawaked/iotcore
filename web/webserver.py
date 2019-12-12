fa2f4b1fdc1e6522d338d4f21286f9968095455a
gerben bunt <g.bunt@st.hanze.nl>
2019-12-12 22:18:48 +0100

    dfsdfs

--- web/webserver.py
+++ web/webserver.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 import sqlite3
+
+from flask import Flask
+from flask_mqtt import Mqtt
+
+app.config['MQTT_BROKER_URL'] = '192.168.1.178'  #homeip
+app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
+# app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+# app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+# app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+
+mqtt = Mqtt()
+
 
 # import paho.mqtt.client as paho
 # broker="192.168.1.184"
